780800df569ccd87f3b74778602e15c43a3f9a4c
Sparrow0hawk <alexjcoleman@gmail.com>
2024-01-11 20:33:48 +0000

    Refactor routes into resource configuration

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{guard, web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -6,11 +6,26 @@ pub struct HelloData {
     pub text: String,
 }
 
-pub async fn index_html() -> impl Responder {
+pub fn index_html_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/").route(web::get().to(index_html)));
+}
+
+pub fn index_json_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/")
+            .guard(
+                guard::All(guard::Header("Accept", "application/json"))
+                    .and(guard::Not(guard::Header("Accept", "text/html"))),
+            )
+            .route(web::get().to(index_json)),
+    );
+}
+
+async fn index_html() -> impl Responder {
     HttpResponse::Ok().body("<h1>Hello world!</h1>")
 }
 
-pub async fn index_json() -> impl Responder {
+async fn index_json() -> impl Responder {
     let hello = HelloData {
         text: String::from("Hello world!"),
     };
--- src/startup.rs
+++ src/startup.rs
@@ -1,20 +1,16 @@
 use std::net::TcpListener;
 
-use crate::routes::{index_html, index_json, not_found};
+use crate::routes::{index_html_config, index_json_config, not_found};
 
 use actix_web::dev::Server;
 use actix_web::middleware::Logger;
-use actix_web::{guard, web, App, HttpServer};
+use actix_web::{web, App, HttpServer};
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(move || {
         App::new()
-            .service(
-                web::scope("/")
-                    .guard(guard::All(guard::Header("Accept", "application/json")).and(guard::Not(guard::Header("Accept", "text/html"))))
-                    .route("", web::get().to(index_json)),
-            )
-            .service(web::scope("/").route("", web::get().to(index_html)))
+            .configure(index_json_config)
+            .configure(index_html_config)
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
             .default_service(web::get().to(not_found))
